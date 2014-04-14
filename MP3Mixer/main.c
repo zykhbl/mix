586448731f9c7b98b427e6773d0ce71a6be222d3
zykhbl2002 <409054740>
2014-04-14 10:19:22 +0800

    Initial Commit

---
+++ MP3Mixer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MP3Mixer
+//
+//  Created by zykhbl on 14-4-14.
+//  Copyright (c) 2014年 zykhbl. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
